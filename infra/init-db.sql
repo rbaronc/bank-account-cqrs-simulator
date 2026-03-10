ae5c10a4d4fec62621c7e364e174ab64a3ee7b25
Rafael Baron <rask2208@gmail.com>
2026-03-10 14:40:37 -0500

    First infra script

---
+++ infra/init-db.sql
@@ -0,0 +1,5 @@
+-- Create databases for each microservice domain
+CREATE DATABASE user_service_db;
+CREATE DATABASE account_command_db;
+CREATE DATABASE account_query_db;
+CREATE DATABASE saga_orchestrator_db;
